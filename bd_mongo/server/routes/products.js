95eb3069ab6aab83ed22c887b29a5dccca6999a7
g-rog649 <g.rogozinski.649@studms.ug.edu.pl>
2023-01-13 15:55:23 +0100

    Add product, remove all products

---
+++ bd_mongo/server/routes/products.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const productRoutes = express.Router();
+const dbo = require("../db/conn");
+const ObjectId = require("mongodb").ObjectId;
+
+const productFields = {
+  name: "string",
+  price: "number",
+  description: "string",
+  amount: "number",
+  unit: "string",
+};
+
+// Get all the products from the database
+productRoutes.route("/products").get((req, res) => {
+  const dbConnect = dbo.getDb("shop");
+  console.log(req.query);
+  const routeOptions = Object.keys(productFields).map((field) => [
+    field,
+    req.query[field],
+  ]);
+  const projectOptions = {};
+  const sortOptions = {};
+  let projectEmpty = true;
+  let sortEmpty = true;
+
+  routeOptions.forEach(([field, option]) => {
+    switch (option) {
+      case "hide":
+        projectOptions[field] = 0;
+        projectEmpty = false;
+        break;
+
+      case "show":
+        projectOptions[field] = 1;
+        projectEmpty = false;
+        break;
+
+      case "asc":
+        sortOptions[field] = 1;
+        sortEmpty = false;
+        break;
+
+      case "desc":
+        sortOptions[field] = -1;
+        sortEmpty = false;
+        break;
+    }
+  });
+
+  const options = [];
+
+  if (!projectEmpty && !sortEmpty) {
+    for (const field in sortOptions) {
+      projectOptions[field] = 1;
+    }
+  }
+  if (!projectEmpty) options.push({ $project: projectOptions });
+  if (!sortEmpty) options.push({ $sort: sortOptions });
+
+  dbConnect
+    .collection("products")
+    .aggregate(options)
+    .toArray((err, response) => {
+      if (err) throw err;
+      res.json(response);
+    });
+});
+
+// Add a product to the database
+productRoutes.route("/product/add").post((req, res) => {
+  const dbConnect = dbo.getDb("shop");
+
+  const addedProduct = Object.fromEntries(
+    Object.keys(productFields).map((field) => [field, req.body[field]])
+  );
+
+  dbConnect.collection("products").insertOne(addedProduct, (err, response) => {
+    if (err) throw err;
+    res.json(response);
+  });
+});
+
+// Remove all products from the database
+productRoutes.route("/products").delete((req, res) => {
+  const dbConnect = dbo.getDb("products");
+
+  dbConnect.collection("products").deleteMany({}, (err, response) => {
+    if (err) throw err;
+    console.log("Deleted all products");
+    res.json(response);
+  });
+});
+
+module.exports = productRoutes;
